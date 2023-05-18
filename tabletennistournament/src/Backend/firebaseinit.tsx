0f903b986d26811178d0d0d364b0e1f016186e26
Jacob DIllström <78909969+Dillenzz@users.noreply.github.com>
2023-05-18 20:37:40 +0200

    i dont care anymore about safety

--- tabletennistournament/src/Backend/firebaseinit.tsx
+++ tabletennistournament/src/Backend/firebaseinit.tsx
@@ -2,20 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC0fqdkCB4Zpq1tkqJjbLdOPipaXZ6AdtE",
-    
-    authDomain: "firsttoeleven-b5ade.firebaseapp.com",
-    projectId: "firsttoeleven-b5ade",
-    storageBucket: "firsttoeleven-b5ade.appspot.com",
-    messagingSenderId: "97616391413",
-    appId: "1:97616391413:web:3f4750771aa72fa0cd29bc",
-    measurementId: "G-LPMMJFBVR7",
-    databaseURL: "https://firsttoeleven-b5ade-default-rtdb.europe-west1.firebasedatabase.app"
+  apiKey: "AIzaSyBpe3B-61BmMQjWDO47CB3PhU3DYzHd3cQ",
+  authDomain: "ft11-cb5a6.firebaseapp.com",
+  projectId: "ft11-cb5a6",
+  storageBucket: "ft11-cb5a6.appspot.com",
+  messagingSenderId: "394898517371",
+  appId: "1:394898517371:web:f4382831514e678c3a61de",
+  measurementId: "G-M3X2JJ11HQ",
+  databaseURL:
+    "https://ft11-cb5a6-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 export const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 export default db;
-
-
